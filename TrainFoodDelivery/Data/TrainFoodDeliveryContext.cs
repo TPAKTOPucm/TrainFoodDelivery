f9e27977fb18a50cf50aff67023b556b5d4e8fc3
TPAKTOPucm <danila24safonov@mail.ru>
2024-04-30 16:52:49 +0300

    Done DbContext

--- TrainFoodDelivery/Data/TrainFoodDeliveryContext.cs
+++ TrainFoodDelivery/Data/TrainFoodDeliveryContext.cs
@@ -1,5 +1,22 @@
-﻿namespace TrainFoodDelivery.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TrainFoodDelivery.Models;
 
-public class TrainFoodDeliveryContext
+namespace TrainFoodDelivery.Data;
+
+public class TrainFoodDeliveryContext: DbContext
 {
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<Train> Trains { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public TrainFoodDeliveryContext(DbContextOptions options): base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<WagonProduct>().HasKey(wp => new { wp.TrainNumber, wp.WagonNumber, wp.ProductId });
+        modelBuilder.Entity<ProductOrder>().HasKey(po => new { po.ProductId, po.OrderId });
+    }
 }
