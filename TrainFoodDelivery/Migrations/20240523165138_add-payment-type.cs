22a9dd41860bb0e5b8e31c1b1bb60146a6e4a49f
Danila Safonov <danila24safonov@mail.ru>
2024-05-23 19:52:58 +0300

    migration

---
+++ TrainFoodDelivery/Migrations/20240523165138_add-payment-type.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainFoodDelivery.Migrations
+{
+    public partial class addpaymenttype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentType",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentType",
+                table: "Orders");
+        }
+    }
+}
